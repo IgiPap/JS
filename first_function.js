a4ee5883211d05f3f98f092b3f12af2f35e5be97
Olek H <alek.hus@gmail.com>
2017-01-02 21:32:25 +0100

    add updated first function

--- first_function.js
+++ first_function.js
@@ -1,18 +1,11 @@
-
-var triangle1Area = getTriangleArea(10, 15);
-var triangle2Area = getTriangleArea(0, 15);
-var triangle3Area = getTriangleArea(2, -15);
+getTriangleArea(10, 15);
+getTriangleArea(0, 15);
+getTriangleArea(2, -15);
 
 function getTriangleArea(a, h) {
-	if (a > 0 && h > 0) {
-		var result = a*h/2;
-		console.log(result);
-	} else if (a <= 0 || h <= 0) {
-		console.log('Nieprawidłowe dane');
+    if (a > 0 && h > 0) {
+		console.log(a * h / 2);
+		return;
 	}
-	return result;
+	console.log('Nieprawidłowe dane');
 }
-
-
-
-
