934df810db9926d8f3abf3bb023cb39235cee6d7
Olek H <alek.hus@gmail.com>
2017-01-03 12:45:45 +0100

    function added

--- first_loop.js
+++ first_loop.js
@@ -1,9 +1,13 @@
 
 var treeHeight = 5;
 
-for (var index = 1; index <= treeHeight; index++) {
-	var level = '';
-	for (var smallIndex = 1; smallIndex <= index * 2 - 1; smallIndex++) {
-	level += '*';
-	} console.log(level);
+function christmasTree () {
+	for (var index = 1; index <= treeHeight; index++) {
+		var level = '';
+		for (var smallIndex = 1; smallIndex <= index * 2 - 1; smallIndex++) {
+		level += '*';
+		} console.log(level);
+	}
 }
+
+christmasTree();
