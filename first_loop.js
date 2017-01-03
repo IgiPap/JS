03a3a356cededa92c12c19f28ba5768fcea51563
Olek H <alek.hus@gmail.com>
2017-01-03 11:02:45 +0100

    poprawiona klamra

--- first_loop.js
+++ first_loop.js
@@ -5,5 +5,5 @@ for (var index = 1; index <= treeHeight; index++) {
 	var level = '';
 	for (var smallIndex = 1; smallIndex <= index * 2 - 1; smallIndex++) {
 	level += '*';
-		} console.log(level);
+	} console.log(level);
 }
