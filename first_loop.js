4803211a11d8ec07e17924cdc431d8089fa87fca
Olek H <alek.hus@gmail.com>
2017-01-10 07:14:05 +0100

    function param added

--- first_loop.js
+++ first_loop.js
@@ -1,13 +1,14 @@
 
-var treeHeight = 5;
+var treeHeight = 7;
 
-function christmasTree () {
+function christmasTree (treeHeight) {
 	for (var index = 1; index <= treeHeight; index++) {
 		var level = '';
 		for (var smallIndex = 1; smallIndex <= index * 2 - 1; smallIndex++) {
 		level += '*';
-		} console.log(level);
+		} 
+        console.log(level);
 	}
 }
 
-christmasTree();
+christmasTree(treeHeight);
